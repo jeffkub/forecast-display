cc5b50c105454c98d12a06bff0f7f7e8163fcf22
jeffkub <jeffkub@gmail.com>
2017-09-29 21:31:30 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,34 @@
+from datetime import datetime
+from PIL import Image, ImageDraw, ImageFont
+
+DISP_SIZE = (264, 176)
+
+DISP_PALETTE = [
+    255, 255, 255,
+      0,   0,   0,
+    255,   0,   0
+]
+
+WHITE = 0
+BLACK = 1
+RED = 2
+
+
+def main():
+    font = ImageFont.truetype('fonts/gnu-freefont-freemono/FreeMonoBold.ttf', 32)
+    font_large = ImageFont.truetype('fonts/gnu-freefont-freemono/FreeMonoBold.ttf', 48)
+
+    im = Image.new('P', DISP_SIZE)
+    im.putpalette(DISP_PALETTE)
+
+    now = datetime.now()
+
+    draw = ImageDraw.ImageDraw(im)
+    draw.text((0, 40), now.strftime('%A'), BLACK, font_large)
+    draw.text((0, 88), now.strftime('%B %d'), BLACK, font)
+
+    im.show()
+
+
+if __name__ == '__main__':
+    main()
