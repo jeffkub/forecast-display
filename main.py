eb14b81938fa28b1f92fbc852f7dda4f01911f52
jeffkub <jeffkub@gmail.com>
2017-10-01 12:51:09 -0400

    Able to select debug show mode for running on pc

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import argparse
 from datetime import datetime
 from PIL import Image, ImageDraw, ImageFont
 
@@ -15,6 +16,12 @@ RED = 2
 
 
 def main():
+    # Parse command line arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--debug_show', action='store_true')
+
+    args = parser.parse_args()
+
     font = ImageFont.truetype('fonts/freefont/FreeMonoBold.ttf', 32)
     font_large = ImageFont.truetype('fonts/freefont/FreeMonoBold.ttf', 48)
 
@@ -25,15 +32,18 @@ def main():
 
     draw = ImageDraw.ImageDraw(im)
     draw.text((0, 40), now.strftime('%A'), BLACK, font_large)
-    draw.text((0, 88), now.strftime('%B %d'), BLACK, font)
+    draw.text((0, 88), now.strftime('%B %d'), RED, font)
 
-#    im.show()
-
-    import epd2in7b
-    epd = epd2in7b.EPD()
-    epd.init()
-    epd.display_image(im.transpose(Image.ROTATE_90), BLACK, RED)
-    epd.sleep()
+    if args.debug_show:
+        # Show image in a window for debugging
+        im.show()
+    else:
+        # Show on e-paper display
+        import epd2in7b
+        epd = epd2in7b.EPD()
+        epd.init()
+        epd.display_image(im.transpose(Image.ROTATE_90), BLACK, RED)
+        epd.sleep()
 
 
 if __name__ == '__main__':
