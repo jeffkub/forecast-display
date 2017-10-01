96821364b3490d7e8bd72ea66172f404ca4d950d
jeffkub <jeffkub@gmail.com>
2017-10-01 10:41:52 -0400

    Able to display image

--- epd2in7b.py
+++ epd2in7b.py
@@ -25,9 +25,6 @@
  #
 
 import epdif
-import Image
-import ImageDraw
-import ImageFont
 import RPi.GPIO as GPIO
 
 # Display resolution
@@ -258,24 +255,6 @@ class EPD:
         for count in range(0, 42):
             self.send_data(self.lut_wb[count])
 
-    def get_frame_buffer(self, image):
-        buf = [0xFF] * (self.width * self.height / 8)
-        # Set buffer to value of Python Imaging Library image.
-        # Image must be in mode 1.
-        image_monocolor = image.convert('1')
-        imwidth, imheight = image_monocolor.size
-        if imwidth != self.width or imheight != self.height:
-            raise ValueError('Image must be same dimensions as display \
-                ({0}x{1}).' .format(self.width, self.height))
-
-        pixels = image_monocolor.load()
-        for y in range(self.height):
-            for x in range(self.width):
-                # Set the bits for the column of pixels at the current position.
-                if pixels[x, y] != 0:
-                    buf[(x + y * self.width) / 8] &= ~(0x80 >> (x % 8))
-        return buf
-
     def display_frame(self, frame_buffer_black, frame_buffer_red):
         self.send_command(TCON_RESOLUTION)
         self.send_data(EPD_WIDTH >> 8)
@@ -286,13 +265,13 @@ class EPD:
         if (frame_buffer_black != None):
             self.send_command(DATA_START_TRANSMISSION_1)           
             self.delay_ms(2)
-            for i in range(0, self.width * self.height / 8):
+            for i in range(0, int(self.width * self.height / 8)):
                 self.send_data(frame_buffer_black[i])  
             self.delay_ms(2)                  
         if (frame_buffer_red != None):
             self.send_command(DATA_START_TRANSMISSION_2)
             self.delay_ms(2)
-            for i in range(0, self.width * self.height / 8):
+            for i in range(0, int(self.width * self.height / 8)):
                 self.send_data(frame_buffer_red[i])  
             self.delay_ms(2)        
 
@@ -326,168 +305,4 @@ class EPD:
             self.width = EPD_HEIGHT
             self.height = EPD_WIDTH
 
-    def set_pixel(self, frame_buffer, x, y, colored):
-        if (x < 0 or x >= self.width or y < 0 or y >= self.height):
-            return
-        if (self.rotate == ROTATE_0):
-            self.set_absolute_pixel(frame_buffer, x, y, colored)
-        elif (self.rotate == ROTATE_90):
-            point_temp = x
-            x = EPD_WIDTH - y
-            y = point_temp
-            self.set_absolute_pixel(frame_buffer, x, y, colored)
-        elif (self.rotate == ROTATE_180):
-            x = EPD_WIDTH - x
-            y = EPD_HEIGHT- y
-            self.set_absolute_pixel(frame_buffer, x, y, colored)
-        elif (self.rotate == ROTATE_270):
-            point_temp = x
-            x = y
-            y = EPD_HEIGHT - point_temp
-            self.set_absolute_pixel(frame_buffer, x, y, colored)
-    
-    def set_absolute_pixel(self, frame_buffer, x, y, colored):
-        # To avoid display orientation effects
-        # use EPD_WIDTH instead of self.width
-        # use EPD_HEIGHT instead of self.height
-        if (x < 0 or x >= EPD_WIDTH or y < 0 or y >= EPD_HEIGHT):
-            return
-        if (colored):
-            frame_buffer[(x + y * EPD_WIDTH) / 8] |= 0x80 >> (x % 8)
-        else:
-            frame_buffer[(x + y * EPD_WIDTH) / 8] &= ~(0x80 >> (x % 8))
-
-    def draw_string_at(self, frame_buffer, x, y, text, font, colored):
-        image = Image.new('1', (self.width, self.height))
-        draw = ImageDraw.Draw(image)
-        draw.text((x, y), text, font = font, fill = 255)
-        # Set buffer to value of Python Imaging Library image.
-        # Image must be in mode 1.
-        pixels = image.load()
-        for y in range(self.height):
-            for x in range(self.width):
-                # Set the bits for the column of pixels at the current position.
-                if pixels[x, y] != 0:
-                    self.set_pixel(frame_buffer, x, y, colored)
-
-    def draw_line(self, frame_buffer, x0, y0, x1, y1, colored):
-        # Bresenham algorithm
-        dx = abs(x1 - x0)
-        sx = 1 if x0 < x1 else -1
-        dy = -abs(y1 - y0)
-        sy = 1 if y0 < y1 else -1
-        err = dx + dy
-        while((x0 != x1) and (y0 != y1)):
-            self.set_pixel(frame_buffer, x0, y0 , colored)
-            if (2 * err >= dy):
-                err += dy
-                x0 += sx
-            if (2 * err <= dx):
-                err += dx
-                y0 += sy
-
-    def draw_horizontal_line(self, frame_buffer, x, y, width, colored):
-        for i in range(x, x + width):
-            self.set_pixel(frame_buffer, i, y, colored)
-
-    def draw_vertical_line(self, frame_buffer, x, y, height, colored):
-        for i in range(y, y + height):
-            self.set_pixel(frame_buffer, x, i, colored)
-
-    def draw_rectangle(self, frame_buffer, x0, y0, x1, y1, colored):
-        min_x = x0 if x1 > x0 else x1
-        max_x = x1 if x1 > x0 else x0
-        min_y = y0 if y1 > y0 else y1
-        max_y = y1 if y1 > y0 else y0
-        self.draw_horizontal_line(frame_buffer, min_x, min_y, max_x - min_x + 1, colored)
-        self.draw_horizontal_line(frame_buffer, min_x, max_y, max_x - min_x + 1, colored)
-        self.draw_vertical_line(frame_buffer, min_x, min_y, max_y - min_y + 1, colored)
-        self.draw_vertical_line(frame_buffer, max_x, min_y, max_y - min_y + 1, colored)
-
-    def draw_filled_rectangle(self, frame_buffer, x0, y0, x1, y1, colored):
-        min_x = x0 if x1 > x0 else x1
-        max_x = x1 if x1 > x0 else x0
-        min_y = y0 if y1 > y0 else y1
-        max_y = y1 if y1 > y0 else y0
-        for i in range(min_x, max_x + 1):
-            self.draw_vertical_line(frame_buffer, i, min_y, max_y - min_y + 1, colored)
-
-    def draw_circle(self, frame_buffer, x, y, radius, colored):
-        # Bresenham algorithm
-        x_pos = -radius
-        y_pos = 0
-        err = 2 - 2 * radius
-        if (x >= self.width or y >= self.height):
-            return
-        while True:
-            self.set_pixel(frame_buffer, x - x_pos, y + y_pos, colored)
-            self.set_pixel(frame_buffer, x + x_pos, y + y_pos, colored)
-            self.set_pixel(frame_buffer, x + x_pos, y - y_pos, colored)
-            self.set_pixel(frame_buffer, x - x_pos, y - y_pos, colored)
-            e2 = err
-            if (e2 <= y_pos):
-                y_pos += 1
-                err += y_pos * 2 + 1
-                if(-x_pos == y_pos and e2 <= x_pos):
-                    e2 = 0
-            if (e2 > x_pos):
-                x_pos += 1
-                err += x_pos * 2 + 1
-            if x_pos > 0:
-                break
-
-    def draw_filled_circle(self, frame_buffer, x, y, radius, colored):
-        # Bresenham algorithm
-        x_pos = -radius
-        y_pos = 0
-        err = 2 - 2 * radius
-        if (x >= self.width or y >= self.height):
-            return
-        while True:
-            self.set_pixel(frame_buffer, x - x_pos, y + y_pos, colored)
-            self.set_pixel(frame_buffer, x + x_pos, y + y_pos, colored)
-            self.set_pixel(frame_buffer, x + x_pos, y - y_pos, colored)
-            self.set_pixel(frame_buffer, x - x_pos, y - y_pos, colored)
-            self.draw_horizontal_line(frame_buffer, x + x_pos, y + y_pos, 2 * (-x_pos) + 1, colored);
-            self.draw_horizontal_line(frame_buffer, x + x_pos, y - y_pos, 2 * (-x_pos) + 1, colored);
-            e2 = err
-            if (e2 <= y_pos):
-                y_pos += 1
-                err += y_pos * 2 + 1
-                if(-x_pos == y_pos and e2 <= x_pos):
-                    e2 = 0
-            if (e2 > x_pos):
-                x_pos  += 1
-                err += x_pos * 2 + 1
-            if x_pos > 0:
-                break
-
 ### END OF FILE ###
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from PIL import Image, ImageDraw, ImageFont
+import epd2in7b
 
 DISP_SIZE = (264, 176)
 
@@ -12,6 +13,22 @@ DISP_PALETTE = [
 WHITE = 0
 BLACK = 1
 RED = 2
+
+
+def create_frame(image):
+    width, height = image.size
+    pixels = image.load()
+    frame_black = [0] * int(width * height / 8)
+    frame_red = [0] * int(width * height / 8)
+
+    for y in range(0, height):
+        for x in range(0, width):
+            if pixels[x, y] == BLACK:
+                frame_black[int((x + y * width) / 8)] |= 0x80 >> (x % 8)
+            elif pixels[x, y] == RED:
+                frame_red[int((x + y * width) / 8)] |= 0x80 >> (x % 8)
+
+    return frame_black, frame_red
 
 
 def main():
@@ -27,7 +44,14 @@ def main():
     draw.text((0, 40), now.strftime('%A'), BLACK, font_large)
     draw.text((0, 88), now.strftime('%B %d'), BLACK, font)
 
-    im.show()
+    rot = im.transpose(Image.ROTATE_90)
+    rot.show()
+
+    frame_black, frame_red = create_frame(rot)
+
+    epd = epd2in7b.EPD()
+    epd.init()
+    epd.display_frame(frame_black, frame_red)
 
 
 if __name__ == '__main__':
