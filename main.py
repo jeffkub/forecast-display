1e165ea19111f3981ebc30ca4807dcb1c62aca6f
jeffkub <jeffkub@gmail.com>
2017-10-03 23:33:54 -0400

    Added config file

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import argparse
 from datetime import datetime
+import json
 import os
 from PIL import Image, ImageDraw, ImageFont
 
@@ -15,22 +16,35 @@ WHITE = 0
 BLACK = 1
 RED = 2
 
+BASE_PATH = os.path.dirname(os.path.abspath(__file__))
+
+
+def get_config():
+    # Parse command line arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config_file', '-c', default=BASE_PATH + '/config.json')
+    parser.add_argument('--debug_show', action='store_true')
+    args = vars(parser.parse_args())
+
+    # Parse from config file
+    with open(args['config_file']) as file:
+        json_data = json.load(file)
+        args.update(json_data)
+
+    return args
+
+
 def center_text(draw, xy, box, text, fill, font):
     text_size = draw.textsize(text, font)
     pos = tuple(map(lambda pos, box, text_size: pos + (box - text_size)/2, xy, box, text_size))
     draw.text(pos, text, fill, font)
 
+
 def main():
-    os.chdir(os.path.dirname(os.path.abspath(__file__)))
+    config = get_config()
 
-    # Parse command line arguments
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--debug_show', action='store_true')
-
-    args = parser.parse_args()
-
-    font = ImageFont.truetype('fonts/freefont/FreeMonoBold.ttf', 32)
-    font_large = ImageFont.truetype('fonts/freefont/FreeMonoBold.ttf', 48)
+    font = ImageFont.truetype(BASE_PATH + '/fonts/freefont/FreeMonoBold.ttf', 32)
+    font_large = ImageFont.truetype(BASE_PATH + '/fonts/freefont/FreeMonoBold.ttf', 48)
 
     im = Image.new('P', DISP_SIZE)
     im.putpalette(DISP_PALETTE)
@@ -41,7 +55,7 @@ def main():
     center_text(draw, (0, 40), (264, 48), now.strftime('%A'), BLACK, font_large)
     center_text(draw, (0, 88), (264, 32), now.strftime('%B %d'), RED, font)
 
-    if args.debug_show:
+    if config['debug_show']:
         # Show image in a window for debugging
         im.show()
     else:
